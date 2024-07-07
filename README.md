7663d257a628e9202d1558cdc8fdaafe1d845026
kongxiangrun <kongxiangrun@126.com>
2024-07-07 18:11:19 +0800

    c

---
+++ README.md
@@ -0,0 +1,11 @@
+ 
+纯前端展示 pc 浏览
+
+
+[流程 vue 18年做的 没有后台无法运行只能 展示节点拖拽等功能](flow/index.html) 
+
+[模板助手 angularjs 15年做的](temp/t.html)
+
+[字符拼接 angularjs 15年做的](temp/a.html) 
+
+[桌面 jquery 11年做的](desktop/index.html)
---
+++ page/data.js
@@ -0,0 +1,111 @@
+export default [
+
+
+  {
+
+    classification: '服务',
+    name: 'Nacos',
+    url:"http://192.168.88.17:8848/nacos/",
+    color:'#252a2f'
+  },
+
+  {
+
+    classification: '服务',
+    name: 'Grafana',
+    url:"http://192.168.88.16:3200/?orgId=1",
+    color:'#000000'
+  },
+
+  {
+    classification: '服务',
+    name: 'GitLab',
+    url: 'https://gitlab.robotpen.com/',
+    color:'#292961'
+  },
+  {
+    classification: '服务',
+    name: 'JIRA',
+    url: '',
+    color:'#0747a6'
+  },
+
+
+  // {
+  //   classification: '系统',
+  //   name: '远程会见',
+  //   url: 'http://192.168.103.201:7309/',
+  // },
+  // {
+  //   classification: '系统',
+  //   name: '智慧城市',
+  //   url: 'http://192.168.103.201:9002/user/login'
+  // },
+  // {
+  //   classification: '服务',
+  //   name: 'ChatGpt',
+  //   url: 'https://192.168.103.201/'
+  // },
+  // {
+  //   classification: '系统',
+  //   name: '看守所考核',
+  //   url: 'http://192.168.103.201:6802/'
+  // }
+  // ,
+  // {
+  //   classification: '系统',
+  //   name: '狱警考核',
+  //   url: 'http://192.168.103.202:6902/'
+  // },
+  // {
+  //   classification: '系统',
+  //   name: '东航',
+  //   url: 'http://192.168.103.201:7609/emmc/'
+  // },
+  //
+  //
+  // {
+  //   classification: '系统',
+  //   name: 'Aiot',
+  //   url: 'http://192.168.103.201:7209/user/login'
+  // },
+  //
+  // {
+  //   classification: '系统',
+  //   name: '海航学院管理系统',
+  //   url: 'http://192.168.103.202:9802/'
+  // },
+  // {
+  //   classification: '系统',
+  //   name:'青岛展厅',
+  //   url:'http://192.168.103.202:16801/'
+  // },
+  // {
+  //   classification: '系统',
+  //   name:'智慧农业',
+  //   url:'http://192.168.103.202:9800/'
+  // },
+  // {
+  //   classification: '系统',
+  //   name:'工业园区',
+  //   url:'http://192.168.103.202:9702/'
+  // },
+  // {
+  //   classification: '原型',
+  //   name:'狱警考核',
+  //   url:'https://modao.cc/app/7Jxc6qHrsiy7wqE0Tbmuv#screen=slfmbkbgy4ti31o'
+  // },
+  {
+    classification: '蓝湖',
+    name:'青岛屏幕',
+    url:'https://lanhuapp.com/link/#/invite?sid=lx0TFj0o'
+  },
+
+
+  {
+    classification: '工具',
+    name:'JSON格式',
+    url:'https://jsoneditoronline.org/'
+  }
+
+]
